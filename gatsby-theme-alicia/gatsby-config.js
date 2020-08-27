c31d352813326dfba9bbf99793c368e20662adb9
Danielkvist <danielkvist@protonmail.com>
2020-08-27 23:01:03 +0200

    chore(gatsby): install gatsby plugins

--- gatsby-theme-alicia/gatsby-config.js
+++ gatsby-theme-alicia/gatsby-config.js
@@ -1,7 +1,23 @@
 module.exports = (options) => {
-	console.log(options);
-
 	return {
-		plugins: [],
+		plugins: [
+			`gatsby-plugin-react-helmet`,
+			`gatsby-plugin-offline`,
+			`gatsby-plugin-manifest`,
+			`gatsby-plugin-styled-components`,
+			`gatsby-plugin-sitemap`,
+			options.googleAnalyticsKey && {
+				resolve: `gatsby-plugin-google-analytics`,
+				options: {
+					trackingId: options.googleAnalyticsKey,
+					head: false,
+					anonymize: true,
+					respectDNT: true,
+					defer: true,
+					sampleRate: 5,
+					siteSpeedSampleRate: 10,
+				},
+			},
+		].filter(Boolean),
 	};
 };
