9b9e0126fa43236cbdf39c61ca3fc1c61a103f4b
Danielkvist <danielkvist@protonmail.com>
2020-08-30 10:01:39 +0200

    feat(app): add hook to use gatsby site metadata

---
+++ gatsby-theme-alicia/src/hooks/use-site-metadata.js
@@ -0,0 +1,20 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const useSiteMetadata = () => {
+	const data = useStaticQuery(graphql`
+		{
+			site {
+				siteMetadata {
+					title
+					description
+					siteUrl
+					author
+				}
+			}
+		}
+	`);
+
+	return data.site.siteMetadata;
+};
+
+export default useSiteMetadata;
