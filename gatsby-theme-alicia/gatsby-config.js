5595205db677ae1d5a88d7d9d6c79c9280af778c
Danielkvist <danielkvist@protonmail.com>
2020-08-30 10:00:45 +0200

    chore(gatsby): remove plugins from theme

--- gatsby-theme-alicia/gatsby-config.js
+++ gatsby-theme-alicia/gatsby-config.js
@@ -1,23 +1,5 @@
 module.exports = (options) => {
 	return {
-		plugins: [
-			`gatsby-plugin-react-helmet`,
-			`gatsby-plugin-offline`,
-			`gatsby-plugin-manifest`,
-			`gatsby-plugin-styled-components`,
-			`gatsby-plugin-sitemap`,
-			options.googleAnalyticsKey && {
-				resolve: `gatsby-plugin-google-analytics`,
-				options: {
-					trackingId: options.googleAnalyticsKey,
-					head: false,
-					anonymize: true,
-					respectDNT: true,
-					defer: true,
-					sampleRate: 5,
-					siteSpeedSampleRate: 10,
-				},
-			},
-		].filter(Boolean),
+		plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
 	};
 };
