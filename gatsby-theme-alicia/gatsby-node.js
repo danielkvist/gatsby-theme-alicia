8c8ac302a180e62b7024daa9568fa0e90c2840bf
Danielkvist <danielkvist@protonmail.com>
2020-08-30 10:01:15 +0200

    chore(gatsby): refactor onPreBootstrap

--- gatsby-theme-alicia/gatsby-node.js
+++ gatsby-theme-alicia/gatsby-node.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 
-exports.onPreBootstrap = ({ store, reporter }, options) => {
+exports.onPreBootstrap = ({ store, reporter }, themeOptions) => {
 	const { program } = store.getState();
-	const content = (options && options.content) || 'posts';
+	const { content = 'content/sections' } = themeOptions;
 
 	if (!fs.existsSync(content)) {
+		reporter.log(`${content} directory not found!`);
 		reporter.log(`creating the ${content} directory`);
-		mkdirp.sync(content);
+
+		mkdirp(content)
+			.then((result) => {
+				reporter.success(`content directory created at ${result}`);
+			})
+			.catch((e) =>
+				reporter.error(`error while creating content directory: ${e}`)
+			);
 	}
 };
